a45050fa45c13d4bab7fa57047d8d1af5326b385
SahidGuardado <sahidguardado@gmail.com>
2024-08-21 16:18:41 -0600

    Add files via upload

---
+++ CalculadoraProyecto/Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+package calculadora;
+/*
+ * @author Cristhian Alvarez
+ */
+public class Calculadora 
+{
+    public static void main(String[] args) 
+    {
+        Calcu calculadora = new Calcu();
+        calculadora.setVisible(true);
+    }
+    
+}
+
+
+ 
+
